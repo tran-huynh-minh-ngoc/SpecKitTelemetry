db9a95d5209935e9d8b87199445907c4140d9ec4
none <none>
2026-04-24 14:03:51 +0700

    Add SessionStart.ps1 and SessionStart.sh

---
+++ scripts/SessionStart.ps1
@@ -0,0 +1,8 @@
+$ErrorActionPreference = "Stop"
+$session_id = ([Console]::In.ReadToEnd() | ConvertFrom-Json).session_id
+
+if ($env:CLAUDE_ENV_FILE) {
+    Add-Content -Path $env:CLAUDE_ENV_FILE -Value "export SESSION_ID=$session_id"
+}
+
+exit 0
